1556dbe4656b31636a6bda61037e9cc01755bfb7
ayoubHam2000 <ayoubbenhamou0731@gmail.com>
2022-04-02 14:47:18 +0000

    projection, 9ble mohammed

--- _cub3d/game.c
+++ _cub3d/game.c
@@ -6,7 +6,7 @@
 /*   By: aben-ham <aben-ham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/31 09:03:21 by aben-ham          #+#    #+#             */
-/*   Updated: 2022/04/02 12:50:32 by aben-ham         ###   ########.fr       */
+/*   Updated: 2022/04/02 14:45:09 by aben-ham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,34 +23,52 @@ void	draw_player()
 	draw_square(pos.x * TILE_SIZE_MAP, pos.y * TILE_SIZE_MAP, TILE_SIZE_MAP, 0xff00ff);
 }
 
+void	draw_vertival_line(int x, int length)
+{
+	int	s;
+	int	e;
+	int color;
+
+	color = ((length * 255) / 100.0);
+	printf("%d\n", color);
+	color %= 255;
+	color = (color << 16) | (color << 8) | color;
+	s = (WIN_SIZE - length) / 2;
+	e = (WIN_SIZE + length) / 2;
+	while (s < e)
+	{
+		if (length)
+		ft_put_pixel(x, s, color);
+		s++;
+	}
+}
+
 void	projection(void)
 {
 	t_player	*p;
 	float		angle;
 	float		max_angle;
 	float		length;
+	float		step;
+	int			i;
 
 	p = &(get_prog()->player);
-	angle = p->angle - 1;
-	max_angle = p->angle + 1;
+	angle = p->angle - p->view_angle / 2;
+	max_angle = p->angle + p->view_angle / 2;
+	step = p->view_angle / WIN_SIZE;
+	i = 0;
+	draw_square(0, 0, WIN_SIZE / 2, 0x0000ff);
+	draw_square(WIN_SIZE / 2, 0, WIN_SIZE / 2, 0x0000ff);
+	draw_square(0, WIN_SIZE / 2, WIN_SIZE / 2, 0x1e1e63);
+	draw_square(WIN_SIZE / 2, WIN_SIZE / 2, WIN_SIZE / 2, 0x1e1e63);
 	while (angle < max_angle)
 	{
 		length = ray_casting(p, angle);
-		draw_line(angle, length, p->x, p->y, 0xff00ff);
-		angle += 0.01;
+		draw_vertival_line(i, 2000 / length);
+		//draw_line(angle, length, p->x, p->y, 0xff00ff);
+		angle += step;
+		i++;
 	}
-}
-
-void	mini_map(t_prog *prog)
-{
-	t_point	size;
-	
-	size.x = get_longest_line_width(prog->map->map) * TILE_SIZE_MAP;
-	size.y = prog->map->len * TILE_SIZE_MAP;
-	replace_image(prog, size.x, size.y);
-	draw_map();
-	draw_player();
-	mlx_put_image_to_window(prog->mlx, prog->win, prog->img.img, WIN_SIZE - size.x, WIN_SIZE - size.y);
 }
 
 void	draw_map_world(void)
@@ -78,11 +96,27 @@ void	draw_map_world(void)
 	}
 }
 
+void	mini_map(t_prog *prog)
+{
+	t_point	size;
+	
+	//size.x = get_longest_line_width(prog->map->map) * TILE_SIZE_MAP;
+	//size.y = prog->map->len * TILE_SIZE_MAP;
+	//replace_image(prog, size.x, size.y);
+	replace_image(prog, WIN_SIZE, WIN_SIZE);
+	draw_map_world();
+	draw_player_in_map();
+	//draw_map();
+	//draw_player();
+	//mlx_put_image_to_window(prog->mlx, prog->win, prog->img.img, WIN_SIZE - size.x, WIN_SIZE - size.y);
+	mlx_put_image_to_window(prog->mlx, prog->win, prog->img.img, WIN_SIZE, 0);
+}
+
 void	game(t_prog *prog)
 {
 	replace_image(prog, WIN_SIZE, WIN_SIZE);
-
-
+	//draw_map_world();
+	projection();
 	mlx_put_image_to_window(prog->mlx, prog->win, prog->img.img, 0, 0);
 }
 
--- _cub3d/game_util.c
+++ _cub3d/game_util.c
@@ -6,9 +6,26 @@
 /*   By: aben-ham <aben-ham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/31 11:35:13 by aben-ham          #+#    #+#             */
-/*   Updated: 2022/04/01 17:31:35 by aben-ham         ###   ########.fr       */
+/*   Updated: 2022/04/02 14:39:34 by aben-ham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
+void	draw_player_in_map()
+{
+	t_player	*p;
+	float		angle;
+	float		max_angle;
+	float		length;
+
+	p = &(get_prog()->player);
+	angle = p->angle - p->view_angle / 2;
+	max_angle = p->angle + p->view_angle / 2;
+	while (angle < max_angle)
+	{
+		length = ray_casting(p, angle);
+		draw_line(angle, length, p->x, p->y, 0xff00ff);
+		angle += 0.01;
+	}
+}
